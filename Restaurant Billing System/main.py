a502ac3925e574bcfb5849ab50c416951203bd6b
Manthan <waghmanthan13@gmail.com>
2024-07-21 13:31:01 +0530

    Some Modifaction in Bill Area

--- Restaurant Billing System/main.py
+++ Restaurant Billing System/main.py
@@ -1,7 +1,5 @@
 from tkinter import *
 import random
-import os
-import sys
 from tkinter import messagebox
 class Bill_App:
     def __init__(self,root):
@@ -154,14 +152,14 @@ class Bill_App:
         total_hygine=Label(billing_menu,text="Total Snacks Price",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=2,column=0)
         total_hygine_entry=Entry(billing_menu,width=30,borderwidth=2,textvariable=self.total_hyg).grid(row=2,column=1,padx=10,pady=7)
 
-        tax_snacks=Label(billing_menu,text="Snacks Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=0,column=2)
+        tax_snacks=Label(billing_menu,text="Starter Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=0,column=2)
         tax_snacks_entry=Entry(billing_menu,width=30,borderwidth=2,textvariable=self.a).grid(row=0,column=3,padx=10,pady=7)
 
-        tax_grocery=Label(billing_menu,text="Grocery Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=1,column=2)
+        tax_grocery=Label(billing_menu,text="Main Course Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=1,column=2)
         tax_grocery_entry=Entry(billing_menu,width=30,borderwidth=2,textvariable=self.b).grid(row=1,column=3,padx=10,pady=7)
 
 
-        tax_hygine=Label(billing_menu,text="Beauty & Hygine Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=2,column=2)
+        tax_hygine=Label(billing_menu,text="Snacks Tax",font=("Arial Black",11),bg="#A569BD",fg="white").grid(row=2,column=2)
         tax_hygine_entry=Entry(billing_menu,width=30,borderwidth=2,textvariable=self.c).grid(row=2,column=3,padx=10,pady=7)
 
         button_frame=Frame(billing_menu,bd=7,relief=GROOVE,bg="#6C3483")
@@ -213,13 +211,13 @@ def total(self):
     self.total_gro.set(str(total_grocery_price)+" Rs")
     self.b.set(str(round(total_grocery_price*0.09,3))+" Rs")
 
-    self.so=self.soap.get()*180
-    self.sh=self.shampoo.get()*180
-    self.cr=self.cream.get()*130
-    self.lo=self.lotion.get()*500
-    self.fo=self.foam.get()*85
-    self.ma=self.mask.get()*100
-    self.sa=self.sanitizer.get()*20
+    self.so=self.Noodles.get()*180
+    self.sh=self.pasta.get()*180
+    self.cr=self.aloo_chaat.get()*100
+    self.lo=self.pav_bhaji.get()*150
+    self.fo=self.bhel_puri.get()*50
+    self.ma=self.paani_puri.get()*50
+    self.sa=self.pakora.get()*80
 
     total_hygine_price=(
             self.so+
@@ -242,7 +240,7 @@ def total(self):
     billarea(self)
 def intro(self):
     self.txtarea.delete(1.0,END)
-    self.txtarea.insert(END,"\tWELCOME TO SUPER MARKET\n\tPhone-No.739275410")
+    self.txtarea.insert(END,"\tWELCOME TO Restaurant\n\tPhone-No.9823968917")
     self.txtarea.insert(END,f"\n\nBill no. : {self.bill_no.get()}")
     self.txtarea.insert(END,f"\nCustomer Name : {self.c_name.get()}")
     self.txtarea.insert(END,f"\nPhone No. : {self.phone.get()}")
@@ -251,81 +249,81 @@ def intro(self):
     self.txtarea.insert(END,"\n====================================\n")
 def billarea(self):
     intro(self)
-    if self.nutella.get()!=0:
-        self.txtarea.insert(END,f"Nutella\t\t {self.nutella.get()}\t{self.nu}\n")
-    if self.noodles.get()!=0:
-        self.txtarea.insert(END,f"Noodles\t\t {self.noodles.get()}\t{self.no}\n")
-    if self.lays.get()!=0:
-        self.txtarea.insert(END,f"Lays\t\t {self.lays.get()}\t{self.la}\n")
-    if self.oreo.get()!=0:
-        self.txtarea.insert(END,f"Oreo\t\t {self.oreo.get()}\t{self.ore}\n")
-    if self.muffin.get()!=0:
-        self.txtarea.insert(END,f"Muffins\t\t {self.muffin.get()}\t{self.mu}\n")
-    if self.silk.get()!=0:
-        self.txtarea.insert(END,f"Silk\t\t {self.silk.get()}\t{self.si}\n")
-    if self.namkeen.get()!=0:
-        self.txtarea.insert(END,f"Namkeen\t\t {self.namkeen.get()}\t{self.na}\n")
-    if self.atta.get()!=0:
-        self.txtarea.insert(END,f"Atta\t\t {self.atta.get()}\t{self.at}\n")
-    if self.pasta.get()!=0:
-        self.txtarea.insert(END,f"Pasta\t\t {self.pasta.get()}\t{self.pa}\n")
-    if self.rice.get()!=0:
-        self.txtarea.insert(END,f"Rice\t\t {self.rice.get()}\t{self.ri}\n")
-    if self.oil.get()!=0:
-        self.txtarea.insert(END,f"Oil\t\t {self.oil.get()}\t{self.oi}\n")
-    if self.sugar.get()!=0:
-        self.txtarea.insert(END,f"Sugar\t\t {self.sugar.get()}\t{self.su}\n")
+    if self.samosa.get()!=0:
+        self.txtarea.insert(END,f"Samosa\t\t {self.samosa.get()}\t{self.nu}\n")
+    if self.paneer_tikka.get()!=0:
+        self.txtarea.insert(END,f"Paneer Tikka\t\t {self.paneer_tikka.get()}\t{self.no}\n")
+    if self.chicken_tikka.get()!=0:
+        self.txtarea.insert(END,f"Chicken Tikka\t\t {self.chicken_tikka.get()}\t{self.la}\n")
+    if self.vegetable_pakora.get()!=0:
+        self.txtarea.insert(END,f"Vegetable Pakoda\t\t {self.vegetable_pakora.get()}\t{self.ore}\n")
+    if self.papdi_chaat.get()!=0:
+        self.txtarea.insert(END,f"Masala Papdi Chaat\t\t {self.papdi_chaat.get()}\t{self.mu}\n")
+    if self.tomato_soup.get()!=0:
+        self.txtarea.insert(END,f"Tomato Soup\t\t {self.tomato_soup.get()}\t{self.na}\n")
+    if self.masala_papad.get()!=0:
+        self.txtarea.insert(END,f"Masala Papad\t\t {self.masala_papad.get()}\t{self.si}\n")
+    if self.butter_chicken.get()!=0:
+        self.txtarea.insert(END,f"Butter Chicken\t\t {self.butter_chicken.get()}\t{self.at}\n")
+    if self.tikka_masala.get()!=0:
+        self.txtarea.insert(END,f"Paneer Tikka Masala\t\t {self.tikka_masala.get()}\t{self.pa}\n")
+    if self.basmathi_rice.get()!=0:
+        self.txtarea.insert(END,f"Jira Rice\t\t {self.basmathi_rice.get()}\t{self.ri}\n")
+    if self.paneer_masala.get()!=0:
+        self.txtarea.insert(END,f"Paneer Masala\t\t {self.paneer_masala.get()}\t{self.oi}\n")
+    if self.palak_paneer.get()!=0:
+        self.txtarea.insert(END,f"Palak Paneer\t\t {self.palak_paneer.get()}\t{self.su}\n")
     if self.dal.get()!=0:
-        self.txtarea.insert(END,f"Daal\t\t {self.dal.get()}\t{self.da}\n")
-    if self.tea.get()!=0:
-        self.txtarea.insert(END,f"Tea\t\t {self.tea.get()}\t{self.te}\n")
-    if self.soap.get()!=0:
-        self.txtarea.insert(END,f"Soap\t\t {self.soap.get()}\t{self.so}\n")
-    if self.shampoo.get()!=0:
-        self.txtarea.insert(END,f"Shampoo\t\t {self.shampoo.get()}\t{self.sh}\n")
-    if self.lotion.get()!=0:
-        self.txtarea.insert(END,f"Lotion\t\t {self.lotion.get()}\t{self.lo}\n")
-    if self.cream.get()!=0:
-        self.txtarea.insert(END,f"Cream\t\t {self.cream.get()}\t{self.cr}\n")
-    if self.foam.get()!=0:
-        self.txtarea.insert(END,f"Foam\t\t {self.foam.get()}\t{self.fo}\n")
-    if self.mask.get()!=0:
-        self.txtarea.insert(END,f"Mask\t\t {self.mask.get()}\t{self.ma}\n")
-    if self.sanitizer.get()!=0:
-        self.txtarea.insert(END,f"Sanitizer\t\t {self.sanitizer.get()}\t{self.sa}\n")
+        self.txtarea.insert(END,f"Daal Tadka\t\t {self.dal.get()}\t{self.da}\n")
+    if self.chole_bhuture.get()!=0:
+        self.txtarea.insert(END,f"Chole Bhuture\t\t {self.chole_bhuture.get()}\t{self.te}\n")
+    if self.Noodles.get()!=0:
+        self.txtarea.insert(END,f"Noodles\t\t {self.Noodles.get()}\t{self.so}\n")
+    if self.aloo_chaat.get()!=0:
+        self.txtarea.insert(END,f"Aloo Chaat\t\t {self.aloo_chaat.get()}\t{self.lo}\n")
+    if self.pasta.get()!=0:
+        self.txtarea.insert(END,f"Pasta\t\t {self.pasta.get()}\t{self.sh}\n")
+    if self.pav_bhaji.get()!=0:
+        self.txtarea.insert(END,f"Pav Bhaji\t\t {self.pav_bhaji.get()}\t{self.cr}\n")
+    if self.bhel_puri.get()!=0:
+        self.txtarea.insert(END,f"Bhel Puri\t\t {self.bhel_puri.get()}\t{self.fo}\n")
+    if self.paani_puri.get()!=0:
+        self.txtarea.insert(END,f"Paani Puri\t\t {self.paani_puri.get()}\t{self.ma}\n")
+    if self.pakora.get()!=0:
+        self.txtarea.insert(END,f"Pakode\t\t {self.pakora.get()}\t{self.sa}\n")
 
     self.txtarea.insert(END,f"------------------------------------\n")
     if self.a.get()!="0.0 Rs":
-        self.txtarea.insert(END,f"Total Snacks Tax : {self.a.get()}\n")
+        self.txtarea.insert(END,f"Total Starter Tax : {self.a.get()}\n")
     if self.b.get()!="0.0 Rs":
-        self.txtarea.insert(END,f"Total Grocery Tax : {self.b.get()}\n")
+        self.txtarea.insert(END,f"Total Main Course Tax : {self.b.get()}\n")
     if self.c.get()!="0.0 Rs":
-        self.txtarea.insert(END,f"Total Beauty&Hygine Tax : {self.c.get()}\n")
+        self.txtarea.insert(END,f"Total Snacks Tax : {self.c.get()}\n")
     self.txtarea.insert(END,f"Total Bill Amount : {self.total_all_bil}\n")
     self.txtarea.insert(END,f"------------------------------------\n")
 def clear(self):
     self.txtarea.delete(1.0,END)
-    self.nutella.set(0)
-    self.noodles.set(0)
-    self.lays.set(0)
-    self.oreo.set(0)
-    self.muffin.set(0)
-    self.silk.set(0)
-    self.namkeen.set(0)
-    self.atta.set(0)
-    self.pasta.set(0)
-    self.rice.set(0)
-    self.oil.set(0)
-    self.sugar.set(0)
+    self.samosa.set(0)
+    self.paneer_tikka.set(0)
+    self.chicken_tikka.set(0)
+    self.vegetable_pakora.set(0)
+    self.papdi_chaat.set(0)
+    self.tomato_soup.set(0)
+    self.masala_papad.set(0)
+    self.butter_chicken.set(0)
+    self.tikka_masala.set(0)
+    self.basmathi_rice.set(0)
+    self.paneer_masala.set(0)
+    self.palak_paneer.set(0)
     self.dal.set(0)
-    self.tea.set(0)
-    self.soap.set(0)
-    self.shampoo.set(0)
-    self.lotion.set(0)
-    self.cream.set(0)
-    self.foam.set(0)
-    self.mask.set(0)
-    self.sanitizer.set(0)
+    self.chole_bhuture.set(0)
+    self.Noodles.set(0)
+    self.aloo_chaat.set(0)
+    self.pasta.set(0)
+    self.pav_bhaji.set(0)
+    self.bhel_puri.set(0)
+    self.paani_puri.set(0)
+    self.pakora.set(0)
     self.total_sna.set(0)
     self.total_gro.set(0)
     self.total_hyg.set(0)
